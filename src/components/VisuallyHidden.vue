aa31d7bef160508650767746e35e49fd86539162
Micha Huhn <michahuhn1@gmx.de>
2024-08-22 18:32:08 +0200

    fix memory leak in `VisuallyHidden.vue` remove event listeners in `onMounted()`

--- src/components/VisuallyHidden.vue
+++ src/components/VisuallyHidden.vue
@@ -1,14 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onUnmounted } from 'vue';
 
 const forceShow = ref(false);
 
 if (!import.meta.env.PROD) {
-  document.addEventListener('keydown', event => {
-    if (event.key === 'AltGraph') {
-      forceShow.value = !forceShow.value;
+  (() => {
+    const TOGGLE_KEY = 'AltGraph';
+    
+    function handleKeyDown(event: KeyboardEvent) {
+      if (event.key === TOGGLE_KEY) {
+        forceShow.value = true;
+      }
     }
-  });
+    
+    function handleKeyUp(event: KeyboardEvent) {
+      if (event.key === TOGGLE_KEY) {
+        forceShow.value = false;
+      }
+    }
+    
+    document.addEventListener('keydown', handleKeyDown);
+    document.addEventListener('keyup', handleKeyUp)
+    
+    onUnmounted(() => {
+      document.removeEventListener('keydown', handleKeyDown);
+      document.removeEventListener('keyup', handleKeyUp);
+    });
+  })();
 }
 </script>
 
